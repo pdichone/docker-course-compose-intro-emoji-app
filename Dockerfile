0004c9cf8e914d8f2872a578d8d0adda7b0f20c0
Paulo Dichone <pdichone@Paulos-MacBook-Pro.local>
2023-11-08 11:11:51 -0800

    add dockerignore file

--- Dockerfile
+++ Dockerfile
@@ -8,8 +8,6 @@ ENV FLASK_RUN_HOST=0.0.0.0
 
 RUN apk add --no-cache gcc musl-dev linux-headers
 
-
-
 COPY requirements.txt /app/
 RUN pip install --no-cache-dir -r requirements.txt
 
---
+++ share-image-to-dockerhub.sh
@@ -0,0 +1,11 @@
+# Step 1
+# go to hub.docker.com
+# sign up for an account
+# login
+# create a repository
+# give it a name
+# then get the image repository name to use when we create our image (youname/nameofimage)
+# On the right you'll see "To push a new tag to this repository"
+
+# ==== Open terminal/command line to Create our image =====
+
