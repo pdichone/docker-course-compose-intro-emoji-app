903d7d628b5ebdf150d68105cd1c97b819b28a30
Paulo Dichone <pdichone@Paulos-MacBook-Pro.local>
2023-11-08 12:04:11 -0800

    improve Dockerfile and other resources

--- share-image-to-dockerhub.sh
+++ share-image-to-dockerhub.sh
@@ -8,4 +8,17 @@
 # On the right you'll see "To push a new tag to this repository"
 
 # ==== Open terminal/command line to Create our image =====
+# Build your image first
+docker build -t pauloclouddev/random-emoji-app .
+
+# Tag your image
+docker tag pauloclouddev/random-emoji-app:latest pauloclouddev/random-emoji-app:1.0
+
+# Make sure to login to docker hub in the terminal
+# Go through the login process: enter username and password...then you'll be ready to push your image
+
+docker login
+
+# Push image
+docker push yourusername/imagename:tag # add tag if you have one, if none it will default to lastest tag
 
